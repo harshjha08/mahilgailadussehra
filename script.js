15eb22c5911480d93c16d1f55755f3561e4dc538
Harsh Jha <hjha1289@gmail.com>
2026-06-05 16:05:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,36 @@ let menuBtn = document.querySelector("#menuBtn");
 
 menuBtn.addEventListener("click", () => {
     sideBar.classList.toggle("active");
-    console.log("btn clicked");
-    console.log(sideBar.classList);
-
 });
+
+
+let day2roll = document.getElementById("day2-roll");
+let day5roll = document.getElementById("day5-roll");
+let day6roll = document.getElementById("day6-roll");
+let day8roll = document.getElementById("day8-roll");
+function day2rollview(){
+    day2roll.style.display = "flex";
+}
+function day5rollview(){
+    day5roll.style.display = "flex";
+    
+}
+function day6rollview(){
+    day6roll.style.display = "flex";
+  
+}
+function day8rollview(){
+    day8roll.style.display = "flex";
+}
+function closerollview2(){
+    day2roll.style.display = "none";
+}
+function closerollview5(){
+    day5roll.style.display = "none";
+}
+function closerollview6(){
+    day6roll.style.display = "none";
+}
+function closerollview8(){
+    day8roll.style.display = "none";
+}
